e753ce165aaecc96f55950cfa1e62a4600a6e7be
Lukas Riedersberger <lukasac@gmx.de>
2017-07-06 00:56:29 +0200

    uniforms for window, size and position in the quad geometry shader

--- include/core/rendering/canvas.h
+++ include/core/rendering/canvas.h
@@ -13,32 +13,35 @@ namespace eversim {
 
 			class canvas {
 			private:
-				glm::ivec2 resolution;
-				glm::f32mat4 M;
+				glm::ivec2 resolution, position;
 				GLuint tex, fbo_tex;
 				GLuint fbo;
 				//GLuint vaoid, vboid;
-				std::vector<canvas*> children;
+
 
 				void create_framebuffer();
 			public:
+				std::vector<canvas> children;
+
 				explicit canvas();
 				/*
 				Init canvas with empty texture of size ivec2
 				at position defined by the modelmatrix M
 				*/
-				void init(const glm::ivec2& resolution,
-					const glm::f32mat4& M);
+				void init(
+					const glm::ivec2& resolution,
+					const glm::ivec2& position
+				);
 				/*
 				Init canvas with texture defined by path
 				at position defined by the modelmatrix M
 				*/
 				void init(const std::string& path,
-					const glm::f32mat4& M);
-				void draw(const ShaderProgram& program);
+					const glm::ivec2& position);
+				void draw(const ShaderProgram& program,
+					const glm::ivec2& target_resolution
+					);
 				void draw_to_texture(const ShaderProgram& program, GLuint texture);
-				void add_child(canvas*);
-
 			};
 
 		}
--- resources/shader/screen_sized_quad_geometry.glsl
+++ resources/shader/screen_sized_quad_geometry.glsl
@@ -5,15 +5,9 @@ layout(triangle_strip, max_vertices = 4) out;
 
 out vec2 texcoord;
 
-float window_width = 1920;
-float window_height = 1080;
-vec2 window = vec2(window_width,window_height);
-
-int tw = 32;
-int th = 32;
-
-int x = 30;
-int y = 60;
+uniform vec2 window;
+uniform vec2 size;
+uniform vec2 position;
 
 vec2 allign(vec2 v,vec2 size)
 {
@@ -25,10 +19,13 @@ vec2 allign(vec2 v,vec2 size)
 
 void main() 
 {
-	vec2 ur = allign(vec2(x+tw,y+th),window);
-	vec2 ul = allign(vec2(x,y+th),window);
-	vec2 dr = allign(vec2(x+tw,y),window);
-	vec2 dl = allign(vec2(x,y),window);
+	float tw = size.x;
+	float th = size.y;
+	
+	vec2 ur = allign(position+vec2(tw,th),window);
+	vec2 ul = allign(position+vec2(0,th),window);
+	vec2 dr = allign(position+vec2(tw,0),window);
+	vec2 dl = allign(position+vec2(0,0),window);
 	
 	gl_Position = vec4( ur, 0.5, 1.0 );
     texcoord = vec2( 1.0, 1.0 );
@@ -47,23 +44,4 @@ void main()
     EmitVertex();
 
     EndPrimitive(); 
-	
-	/*
-	gl_Position = vec4( 1.0, 1.0, 0.5, 1.0 );
-    texcoord = vec2( 1.0, 1.0 );
-    EmitVertex();
-
-    gl_Position = vec4(-1.0, 1.0, 0.5, 1.0 );
-    texcoord = vec2( 0.0, 1.0 ); 
-    EmitVertex();
-
-    gl_Position = vec4( 1.0,-1.0, 0.5, 1.0 );
-    texcoord = vec2( 1.0, 0.0 ); 
-    EmitVertex();
-
-    gl_Position = vec4(-1.0,-1.0, 0.5, 1.0 );
-    texcoord = vec2( 0.0, 0.0 ); 
-    EmitVertex();
-
-    EndPrimitive(); */
 }
