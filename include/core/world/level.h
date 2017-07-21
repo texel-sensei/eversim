247c2725b23de7583799799edaf3594a0962d50e
texel-sensei <texelsensei@posteo.net>
2017-07-21 12:36:42 +0200

    [Issue #5] Level class
    The level class models a 2D array of tiles, each with a specified width
    and height

---
+++ include/core/world/level.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "core/world/tile.h"
+
+#include <glm/glm.hpp>
+#include <boost/multi_array.hpp>
+
+
+namespace eversim { namespace core { namespace world {
+
+	class level {
+	public:
+		explicit level(glm::ivec2 extents);
+
+		float get_tile_size() const { return tile_size; }
+		void set_tile_size(float f);
+
+		glm::vec2 get_world_size() const;
+		glm::ivec2 get_num_tiles() const;
+
+		tile& get_tile_by_index(glm::ivec2);
+		tile& get_tile_by_pos(glm::vec2);
+
+		tile const& get_tile_by_index(glm::ivec2) const;
+		tile const& get_tile_by_pos(glm::vec2) const;
+
+		glm::ivec2 index_for_pos(glm::vec2) const;
+		glm::vec2 center_of_tile(glm::ivec2) const;
+
+	private:
+		using container = boost::multi_array<tile, 2>;
+
+		container tiles;
+		float tile_size = 1.f;
+	};
+
+}}}
---
+++ src/core/world/level.cpp
@@ -0,0 +1,75 @@
+#include "core/world/level.h"
+#include "core/world/tile.h"
+#include <glm/detail/type_mat.hpp>
+
+using namespace boost;
+using namespace glm;
+
+namespace eversim { namespace core { namespace world {
+
+	level::level(ivec2 size)
+		: tiles(extents[size.x][size.y])
+	{
+		for (auto x = 0; x < get_num_tiles().x; ++x)
+		{
+			for (auto y = 0; y < get_num_tiles().y; ++y)
+			{
+				auto& t = get_tile_by_index({ x,y });
+				t = tile{ {x,y}, tile_size };
+			}
+		}
+	}
+
+	void level::set_tile_size(float f)
+	{
+		tile_size = f;
+		for(auto i = 0u; i < tiles.num_elements(); ++i)
+		{
+			auto& t = tiles.data()[i];
+			t.size(tile_size);
+		}
+	}
+
+	vec2 level::get_world_size() const
+	{
+		return vec2(get_num_tiles()) * tile_size;
+	}
+
+	ivec2 level::get_num_tiles() const
+	{
+		return {tiles.shape()[0], tiles.shape()[1]};
+	}
+
+	tile& level::get_tile_by_index(ivec2 idx)
+	{
+		return tiles[idx.x][idx.y];
+	}
+
+	tile& level::get_tile_by_pos(vec2 pos)
+	{
+		const auto index = index_for_pos(pos);
+		return get_tile_by_index(index);
+	}
+
+	tile const& level::get_tile_by_index(ivec2 idx) const
+	{
+		return tiles[idx.x][idx.y];
+	}
+
+	tile const& level::get_tile_by_pos(vec2 pos) const
+	{
+		const auto index = index_for_pos(pos);
+		return get_tile_by_index(index);
+	}
+
+	ivec2 level::index_for_pos(vec2 p) const
+	{
+		return {p / tile_size};
+	}
+
+	vec2 level::center_of_tile(ivec2 idx) const
+	{
+		return tile_size * vec2(idx) + vec2(tile_size / 2.f);
+	}
+
+}}}
