1972a2805e7111fca866137804984709989f4a4b
texel-sensei <texelsensei@posteo.net>
2017-07-25 09:31:46 +0200

    Moved glm::vec2 comparision testcode into own file

--- test/math_test.cpp
+++ test/math_test.cpp
@@ -1,34 +1,8 @@
 #include "core/utility/math.h"
+#include "test_helper.h"
 #include <catch.hpp>
 
 using namespace eversim::core::utility;
-
-class glm_equals : public Catch::MatcherBase<glm::vec2> {
-public:
-	explicit glm_equals(glm::vec2 v) : expected(v) {}
-
-	bool match(glm::vec2 const& v) const override {
-		return v.x == Approx(expected.x) && v.y == Approx(expected.y);
-	}
-
-	std::string describe() const override
-	{
-		std::ostringstream ss;
-		ss << "is approximate the same as (" << expected.x << ", " << expected.y << ")";
-		return ss.str();
-	}
-private:
-	glm::vec2 expected;
-};
-
-inline glm_equals is_approx(glm::vec2 expected)
-{
-	return glm_equals(expected);
-}
-inline glm_equals is_approx(float x, float y)
-{
-	return glm_equals({x,y});
-}
 
 TEST_CASE("line", "[utility][math]")
 {
