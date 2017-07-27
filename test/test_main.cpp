61ece492b1ad122d82cb44b5d57f648cd752eb05
texel-sensei <texelsensei@posteo.net>
2017-07-27 14:18:55 +0200

    fixed linker errors in test when testing somehting that uses easylogging++

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,2 +1,9 @@
+#include <easylogging++.h>
+INITIALIZE_EASYLOGGINGPP
+
+// easylogging++ provides it's own set of CHECK macros.
+//Those clash with the catch.hpp macros.
+#undef CHECK
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch.hpp>
