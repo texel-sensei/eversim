7742ccf36fa70424cf4a0e88c490b24b62a98024
Lukas Riedersberger <lukasac@gmx.de>
2017-08-04 11:36:59 +0200

    refactoring of renderable_entity

--- src/core/rendering/renderable_entity.cpp
+++ src/core/rendering/renderable_entity.cpp
@@ -14,61 +14,6 @@ namespace eversim {	namespace core { namespace rendering {
 		program(&default_shader), tex(&default_texture), data(&(*default_quadmesh_ptr))
 	{}
 
-	void RenderableEntity::bind() const
-	{
-
-		if(data != nullptr) data->bind();
-		if(tex != nullptr) {
-
-			//TODO bind "in_Texoffset"
-			const auto& uniforms = program->getActiveUniforms();
-			
-			auto has_tex_offset = false;
-			auto has_tex_size = false;
-			auto has_sprite_size = false;
-			const std::string texoffsetname("texoffset");
-			const std::string texsizename("texsize");
-			const std::string spritesizename("spritesize");
-			for(size_t i = 0; i < uniforms.size(); ++i)
-			{
-				const auto& uniform = uniforms.at(i);
-				if(std::get<0>(uniform) == GL_FLOAT_VEC2 &&
-					std::get<1>(uniform) == texoffsetname)
-				{
-					//LOG(INFO) << "upload" << texoffset[0] << "/" << texoffset[1];
-					auto location = program->getUniformLocation(texoffsetname);
-					glUniform2f(location, texoffset[0], texoffset[1]);
-					has_tex_offset = true;
-				}
-
-				else if (std::get<0>(uniform) == GL_FLOAT_VEC2 &&
-					std::get<1>(uniform) == texsizename)
-				{
-					//LOG(INFO) << "upload" << texoffset[0] << "/" << texoffset[1];
-					auto location = program->getUniformLocation(texsizename);
-					glUniform2f(location, texsize[0], texsize[1]);
-					has_tex_size = true;
-				}
-
-				else if (std::get<0>(uniform) == GL_FLOAT_VEC2 &&
-					std::get<1>(uniform) == spritesizename)
-				{
-					//LOG(INFO) << "upload" << texoffset[0] << "/" << texoffset[1];
-					auto location = program->getUniformLocation(spritesizename);
-					glUniform2f(location, spritesize[0], spritesize[1]);
-					has_sprite_size = true;
-				}
-			}
-
-			tex->bind();
-		}
-	}
-
-	void RenderableEntity::draw() const
-	{
-		data->draw();
-	}
-
 	const instanced_entity_information& RenderableEntity::get_instanced_entity_information() const
 	{
 		instanced_entity_information ifo;
@@ -83,6 +28,83 @@ namespace eversim {	namespace core { namespace rendering {
 		ifo.set_texsize(texsize);
 		ifo.set_spritesize(spritesize);
 	}
+
+	glm::fmat3 RenderableEntity::get_M() const { return M; };
+
+	ShaderProgram* RenderableEntity::get_ShaderProgram() const { return program; };
+
+	TextureBase* RenderableEntity::get_Texture() const { return tex; };
+
+	Multibuffer* RenderableEntity::get_Multibuffer() const { return data; };
+
+	void RenderableEntity::set_M(const glm::fmat3& m)
+	{
+		M = m; 
+		touch();
+	};
+
+	void RenderableEntity::set_ShaderProgram(ShaderProgram* p)
+	{
+		program = p; 
+		touch();
+	};
+
+	void RenderableEntity::set_ShaderProgram(ShaderProgram& p)
+	{
+		set_ShaderProgram(&p); 
+		touch();
+	};
+
+	void RenderableEntity::set_Texture(Texture* t)
+	{
+		tex = t;
+		texsize = t->get_resolution();
+		spritesize = texsize;
+		touch();
+	};
+
+	void RenderableEntity::set_Texture(Texture& t) { set_Texture(&t); };
+
+	void RenderableEntity::set_Texture(Spritemap* sm)
+	{
+		tex = sm;
+		texsize = sm->get_texture().get_resolution();
+		spritesize = texsize;
+		touch();
+	};
+
+	void RenderableEntity::set_Texture(Spritemap* sm,
+		const glm::ivec2& offset, const glm::ivec2& resolution)
+	{
+		tex = sm;
+		texoffset = offset;
+		texsize = resolution;
+		spritesize = sm->get_texture().get_resolution();
+		touch();
+	};
+
+	void RenderableEntity::set_Texture(Spritemap& sm)
+	{
+		set_Texture(&sm);
+	};
+
+	void RenderableEntity::set_Texture(Spritemap& sm,
+		const glm::ivec2& offset, const glm::ivec2& resolution)
+	{
+		set_Texture(&sm, offset, resolution);
+	};
+
+	void RenderableEntity::set_Multibuffer(Multibuffer* b)
+	{
+		data = b; 
+		touch();
+	};
+
+	void RenderableEntity::set_Multibuffer(Multibuffer& b)
+	{
+		data = &b; 
+		touch();
+	};
 
 	void  RenderableEntity::set_Position(glm::fvec2 pos)
 	{
@@ -105,6 +127,48 @@ namespace eversim {	namespace core { namespace rendering {
 	glm::fvec2 RenderableEntity::get_Scale() const
 	{
 		return { M[0][0], M[1][1] };
+	}
+
+	void RenderableEntity::default_Multibuffer()
+	{
+		set_Multibuffer(*default_quadmesh_ptr);
+		touch();
+	}
+
+	void RenderableEntity::default_ShaderProgram()
+	{
+		set_ShaderProgram(default_shader);
+		touch();
+	}
+
+	void RenderableEntity::default_Texture()
+	{
+		set_Texture(default_texture);
+		touch();
+	}
+
+	void RenderableEntity::touch()
+	{
+		touched = true;
+	}
+	void RenderableEntity::untouch()
+	{
+		touched = false;
+	};
+
+	bool RenderableEntity::get_touched() const
+	{
+		return touched;
+	}
+
+	void RenderableEntity::set_Type(const entity_type t)
+	{
+		type = t;
+	}
+
+	entity_type RenderableEntity::get_Type() const
+	{
+		return type;
 	}
 
 	void RenderableEntity::default_State()
