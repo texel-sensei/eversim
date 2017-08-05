c20b2ab27343abf3045d1867355f803f84cfd578
texel-sensei <texelsensei@posteo.net>
2017-08-05 15:23:03 +0200

    Added tile loader error cases

--- include/core/world/errors.h
+++ include/core/world/errors.h
@@ -10,11 +10,20 @@ namespace eversim { namespace core { namespace world {
 		UnknownTile,	// Tile name is not known
 	};
 
-	std::error_code make_error_code(level_error);
+	enum class tile_error {
+		// 0 means no error
+		InheritanceCycle = 1, // Cyclic inheritance is not allowed
+		UnknownTile,	// Tile name is not known
+	};
 
+	std::error_code make_error_code(level_error);
+	std::error_code make_error_code(tile_error);
 }}}
 
 namespace std {
 	template<>
 	struct is_error_code_enum<eversim::core::world::level_error> : true_type {};
+
+	template<>
+	struct is_error_code_enum<eversim::core::world::tile_error> : true_type {};
 }
--- src/core/world/errors.cpp
+++ src/core/world/errors.cpp
@@ -21,11 +21,36 @@ namespace eversim { namespace core { namespace world {
 			}
 		};
 
+		struct tile_error_category : std::error_category {
+
+			const char* name() const noexcept override {
+				return "tile error";
+			}
+
+			std::string message(int _Errval) const override {
+				switch(static_cast<tile_error>(_Errval))
+				{
+				case tile_error::InheritanceCycle: 
+					return "cyclic inheritance";
+				case tile_error::UnknownTile:
+					return "unkown tile name";
+				default: 
+					return "(unknown)";
+				}
+			}
+		};
+
 		const level_error_category level_err_cat;
+		const tile_error_category tile_err_cat;
 	}
 	
 	std::error_code make_error_code(level_error err)
 	{
 		return {static_cast<int>(err), level_err_cat};
+	}
+
+	std::error_code make_error_code(tile_error err)
+	{
+		return { static_cast<int>(err), tile_err_cat };
 	}
 }}}
