a57fcf3455b82724e4afc9ab228d1e7c0c9e0544
texel-sensei <texelsensei@posteo.net>
2017-08-30 20:30:31 +0200

    Improved ImGui::Image(Texture) to correclty handle aspect ratio

--- src/editor/imgui/texture_display.cpp
+++ src/editor/imgui/texture_display.cpp
@@ -1,27 +1,20 @@
 #include "editor/imgui/texture_display.h"
 #include "core/rendering/texture.h"
+#include "core/utility/math.h"
+
+using namespace std;
 using namespace eversim::core::rendering;
 
 namespace ImGui {
+
+
 
 	void Image(Texture const& tex, ImVec2 size, bool keep_aspect)
 	{
 		if (keep_aspect) {
 			const auto res = tex.get_resolution();
-			const auto aspect_ratio = float(res.x) / float(res.y);
 
-			const auto new_aspect = float(size.x) / float(size.y);
-
-			if(abs(new_aspect - aspect_ratio) > 0.001)
-			{
-				if(size.x > size.y)
-				{
-					size.y *= new_aspect / aspect_ratio;
-				} else
-				{
-					size.x *=  aspect_ratio / new_aspect;
-				}
-			}
+			size = eversim::core::utility::calculateAspectRatioFit(res, size);
 		}
 		Image(reinterpret_cast<ImTextureID>(tex.get_tex_id()), size, { 0,1 }, { 1,0 });
 	}
